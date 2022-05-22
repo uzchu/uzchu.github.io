1017bea275add13f03425f88e1cd2ffa20d1c929
Jisoo Yu <spdlqjtop@naver.com>
2022-05-23 04:59:13 +0900

    Initial commit

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const min = String(date.getMinutes()).padStart(2, "0");
+  const sec = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${min}:${sec}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
