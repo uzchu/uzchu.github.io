9ac9b114bfbbf5fb624a07af67fcd82ef537fd28
Jisoo Yu <spdlqjtop@naver.com>
2022-06-21 00:39:39 +0900

    Delete momentum

--- 구버전/js/background.js
+++
@@ -1,7 +0,0 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"];
-
-const chosenImage2 = images[Math.floor(Math.random() * images.length)];
-
-const bgImage2 = document.createElement("img");
-
-bgImage2.src = `img/${chosenImage2}`;
